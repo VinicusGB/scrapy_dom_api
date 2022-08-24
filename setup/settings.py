5cd0e9cf3f9ffd9f228ae288338fa516334aaaf5
VinicusGB <viniciusgomesb@gmail.com>
2022-08-24 10:19:26 -0300

    SETUP: Initial commit

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import subprocess
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@x4fzp#+yxee_+j!j9$0_byk(w-ycnkkal3$1+966%m@o9(ulc'
+SECRET_KEY = subprocess.os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
